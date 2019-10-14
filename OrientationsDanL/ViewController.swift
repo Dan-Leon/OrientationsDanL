c8cf9d43c5e3ee9b97c466327f8622ad3cfbd8f1
dleon <dleon@students.mchenry.edu>
2019-10-14 15:30:39 -0500

    Initial Commit

---
+++ OrientationsDanL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrientationsDanL
+//
+//  Created by Daniel Leon on 10/14/19.
+//  Copyright © 2019 Daniel Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
